ceb16ff9048377391ffa70394044a9f6a8a79c5c
SimoneChiodo <simochi.005@gmail.com>
2025-02-25 15:58:43 +0100

    Add Scanner, input user variables, password creation, password output

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,16 +1,33 @@
 package org.lessons.java.security;
 
+// Import Scanner
+import java.util.Scanner;
+
 public class PasswordGenerator {
   public static void main(String[] args) {
-    String nome;
-    String cognome;
-    String colore;
-    int giorno,mese,anno;
+    // Create Scanner
+    Scanner input = new Scanner(System.in);
+
+    // Create and Initialize User Variables
+    System.err.println("Inserisci il tuo nome: ");
+    String nome = input.nextLine();;
+    System.err.println("Inserisci il tuo cognome: ");
+    String cognome = input.nextLine();
+    System.err.println("Inserisci il tuo colore preferito: ");
+    String colore = input.nextLine();
+    System.err.println("Inserisci il giorno in cui sei nato: ");
+    int giorno = input.nextInt();
+    System.err.println("Inserisci il mese in cui sei nato: ");
+    int mese = input.nextInt();
+    System.err.println("Inserisci l'anno in cui sei nato: ");
+    int anno = input.nextInt();
     
 
+    // Create Password
+    String password = nome + "-" + cognome + "-" + colore + "-" + (giorno+mese+anno);
 
-    // String password = nome + "-" + cognome + "-" + colore + "-" + (giorno+mese+anno);
-    // System.err.println("La password e': " + password);
+    // Print Password
+    System.err.println("La tua password personalizzata e': \n" + password);
   }
 }
 
