a177210b6192774af1973d38db7656513d106921
SimoneChiodo <simochi.005@gmail.com>
2025-02-25 15:50:37 +0100

    Add logic

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -2,6 +2,17 @@ package org.lessons.java.security;
 
 public class PasswordGenerator {
   public static void main(String[] args) {
+    String nome;
+    String cognome;
+    String colore;
+    int giorno,mese,anno;
     
+
+
+    // String password = nome + "-" + cognome + "-" + colore + "-" + (giorno+mese+anno);
+    // System.err.println("La password e': " + password);
   }
 }
+
+// Esempio:
+// Pinco-Pallo-magenta-2011
